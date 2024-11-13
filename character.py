e3e7dd5a4ced2643e16e4957c4f89f76c5063c76
Subhash Reddy <subhash8111999@gmail.com>
2024-11-13 00:28:45 -0500

    Adding Player Weapon and Ammo

---
+++ character.py
@@ -0,0 +1,74 @@
+import pygame
+import constants
+import math
+
+class Character:
+    def __init__(self, x, y, mob_animation_list, character_type):
+        self.character_type = character_type
+        self.flip = False
+        self.animation_list = mob_animation_list[character_type]
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+        self.running = True
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = pygame.Rect(0, 0, 40, 40)
+        self.rect.center = (x, y)
+        
+    
+    # move the character
+    def move(self, dx, dy):
+        self.running = False
+        # check if the character is running
+        if dx != 0 or dy != 0:
+            self.running = True
+        # flip the character
+        if dx < 0:
+            self.flip = True
+        elif dx > 0:
+            self.flip = False
+        # diagonal speed check
+        if dx != 0 and dy != 0:
+            dx = dx / math.sqrt(2)
+            dy = dy / math.sqrt(2)
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+    # update the character's animation
+    def update(self):
+        # check what action the character is doing
+        if self.running == True:
+            self.update_action(1)
+        else:
+            self.update_action(0)
+
+        animation_cooldown = 70
+
+        # handle the animation and update the image
+        self.image = self.animation_list[self.action][self.frame_index]
+        # check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        # check if we have run out of frames
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+    # update the character's action (walking or running)
+    def update_action(self, new_action):
+        # check if the new action is different from the previous one
+        if new_action != self.action:
+            self.action = new_action
+            # update the animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
+    # draw the character on the screen
+    def draw(self, surface):
+        flipped_image = pygame.transform.flip(self.image, self.flip, False)
+        if self.character_type == 0:
+            surface.blit(flipped_image, (self.rect.x, self.rect.y - constants.SCALE * constants.CHARACTER_OFFSET))
+        else:
+            surface.blit(flipped_image, self.rect)
+        pygame.draw.rect(surface, constants.RED, self.rect, 1)
--- main.py
+++ main.py
@@ -1,18 +1,123 @@
 import pygame
+import constants
+from character import Character
+from weapon import Weapon
 
+# initialize pygame
 pygame.init() 
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+# create a screen with a width and height and set the caption
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+pygame.display.set_caption("Halloween Game")
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Clock controlling the speed of the game
+clock = pygame.time.Clock()
+
+# define character movement variables
+moving_left = False
+moving_right = False
+moving_up = False
+moving_down = False
+
+# helper function to scale the character image
+def scale_image(image, scale):
+    return pygame.transform.scale(image, (image.get_width() * scale, image.get_height() * scale))
+
+# load weapon image
+weapon_image = scale_image(pygame.image.load('assets/images/weapons/bow.png').convert_alpha(), constants.BOW_SCALE)
+arrow_image = scale_image(pygame.image.load('assets/images/weapons/arrow.png').convert_alpha(), constants.BOW_SCALE)
+
+# load the character image
+mob_animation_list = []
+mob_types = ['elf', 'imp', 'skeleton', 'goblin', 'muddy', 'tiny_zombie', 'big_demon']
+animation_types = ['idle', 'run']
+
+for mob in mob_types:
+    animation_list = []
+    for animation in animation_types:
+        temp_list = []
+        for i in range(4):
+            img = pygame.image.load(f'assets/images/characters/{mob}/{animation}/{i}.png').convert_alpha()
+            img = scale_image(img, constants.CHARACTER_SCALE)
+            temp_list.append(img)
+        animation_list.append(temp_list)
+    mob_animation_list.append(animation_list)
+
+# create a character object
+player = Character(100, 100, mob_animation_list, 0)
+
+# create a weapon object
+bow = Weapon(weapon_image, arrow_image)
+
+# Create sprite group
+arrow_group = pygame.sprite.Group()
 
 # main game loop
 run = True
 while run:
+    # set the frame rate
+    clock.tick(constants.FPS)
+    # fill the screen with a color
+    screen.fill(constants.BGCOLOR)
+
+    # Calculate the character's movement
+    dx = 0
+    dy = 0
+    if moving_left == True:
+        dx = -constants.CHARACTER_SPEED
+    if moving_right == True:
+        dx = constants.CHARACTER_SPEED
+    if moving_up == True:
+        dy = -constants.CHARACTER_SPEED
+    if moving_down == True:
+        dy = constants.CHARACTER_SPEED
+
+    # print(dx, dy)
+
+    # move the character
+    player.move(dx, dy)
+
+    # update the character's animation
+    player.update()
+    arrow = bow.update(player)
+    if arrow:
+        arrow_group.add(arrow)
+    for arrow in arrow_group:
+        arrow.update()
+
+    # draw the character on screen
+    player.draw(screen)
+    bow.draw(screen)
+    for arrow in arrow_group:
+        arrow.draw(screen)
+
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-pygame.quit()
+        # Keydown event
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_w:
+                moving_up = True
+            if event.key == pygame.K_s:
+                moving_down = True
+
+        # KeyUp event
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+            if event.key == pygame.K_w:
+                moving_up = False
+            if event.key == pygame.K_s:
+                moving_down = False
+
+    pygame.display.update()
+
+pygame.quit() 
