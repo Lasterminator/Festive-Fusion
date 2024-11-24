92f0a53a42f005fe9a9493aa3b142708b0b851cc
Subhash Reddy <subhash8111999@gmail.com>
2024-11-24 16:14:01 -0500

    made item update to abstract item

--- items.py
+++ items.py
@@ -20,6 +20,19 @@ class Item(pygame.sprite.Sprite, ABC):
     def collect(self, player, player_score, sound_fx):
         pass
     
+    @abstractmethod
+    def update(self, screen_scroll, player, coin_fx, heal_fx, level, player_score):
+        pass
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+class Coin(Item):
+    def collect(self, player, player_score, sound_fx):
+        player.score += 1
+        player_score += constants.REWARD_MAP['coin']
+        sound_fx.play()
+
     def update(self, screen_scroll, player, coin_fx, heal_fx, level, player_score):
         if not self.dummy_coin:
             self.rect.x += screen_scroll[0]
@@ -31,7 +44,28 @@ class Item(pygame.sprite.Sprite, ABC):
                 player.score += 1
                 player_score += constants.REWARD_MAP['coin']
                 coin_fx.play()
-            elif self.item_type == 1:
+            
+            # Add item to world's collected items list
+            from world import World
+            world = World()
+            if not self.dummy_coin:
+                world.collected_items.add((self.CSV_X, self.CSV_Y))
+                
+            self.kill()
+
+class HealthPotion(Item):
+    def collect(self, player, player_score, sound_fx):
+        player.health = min(player.health + 20, 100)
+        sound_fx.play()
+
+    def update(self, screen_scroll, player, coin_fx, heal_fx, level, player_score):
+        if not self.dummy_coin:
+            self.rect.x += screen_scroll[0]
+            self.rect.y += screen_scroll[1]
+
+        if self.rect.colliderect(player.rect):
+            #coin collected
+            if self.item_type == 1:
                 player.health += 20
                 heal_fx.play()
                 if player.health > 100:
@@ -44,29 +78,7 @@ class Item(pygame.sprite.Sprite, ABC):
                 world.collected_items.add((self.CSV_X, self.CSV_Y))
                 
             self.kill()
-
-        # handle the item animation
-        animation_cooldown = 150
-        self.image = self.animation_list[self.frame_index]
-        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
-            self.frame_index += 1
-            self.update_time = pygame.time.get_ticks()
-        if self.frame_index >= len(self.animation_list):
-            self.frame_index = 0
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
-
-class Coin(Item):
-    def collect(self, player, player_score, sound_fx):
-        player.score += 1
-        player_score += constants.REWARD_MAP['coin']
-        sound_fx.play()
-
-class HealthPotion(Item):
-    def collect(self, player, player_score, sound_fx):
-        player.health = min(player.health + 20, 100)
-        sound_fx.play()
+        
 
 class ItemFactory:
     @staticmethod
